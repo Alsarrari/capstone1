cf3276670392681297533a6c1002380bbc5092d2
abdulrhman.alsarari <abdulrhman.alsarari1@gmail.com>
2025-11-18 20:22:53 +0300

    Update MerchantStock.java

--- src/main/java/com/springbooteventsystem/capstone1/Model/MerchantStock.java
+++ src/main/java/com/springbooteventsystem/capstone1/Model/MerchantStock.java
@@ -1,24 +1,25 @@
 package com.springbooteventsystem.capstone1.Model;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class MerchantStock {
-    @NotEmpty(message = "the id cannot null")
-    private String id ;
-    @NotEmpty(message = "the product cannot null")
-    private String product;
-    @NotEmpty(message = "the merchantid must not be empty")
-    private String merchantid;
-    @NotNull(message = "the stock must be null")
-    @Min(value = 10,message = "have to be more then 10 at start")
+
+    @NotNull(message = "id cannot be null")
+    private String id;
+
+    @NotNull(message = "merchantId cannot be null")
+    private String merchantId;
+
+    @NotNull(message = "productId cannot be null")
+    private String productId;
+
+    @Positive(message = "stock must be a positive number")
     private int stock;
+
 
 
 }
