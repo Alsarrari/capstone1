a746a4d4b2e95e83d2b766451406639c03a5f3ee
abdulrhman.alsarari <abdulrhman.alsarari1@gmail.com>
2025-11-18 20:21:58 +0300

    Refactor User class fields and validation annotations

--- src/main/java/com/springbooteventsystem/capstone1/Model/User.java
+++ src/main/java/com/springbooteventsystem/capstone1/Model/User.java
@@ -6,27 +6,34 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class User {
-    @NotEmpty(message = "the id must not be null ")
-    private String id;
-    @NotEmpty(message = "the username not be null")
-    @Size(min = 5,message = "have to be more then 5 length long")
+public class User {@NotEmpty(message = "the id must not be null ")
+private String id;
 
+    @NotEmpty(message = "the username not be null")
+    @Size(min = 5, message = "have to be more then 5 length long")
     private String userName;
+
     @NotNull(message = "the password not be null")
     @Size(min = 6 , message = "have to be more then 6 length long")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d).+$",
-            message = "password must contain letters and digits")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d).+$",
+            message = "password must contain letters and digits"
+    )
     private String password;
+
     @NotEmpty(message = "email cannot be null")
     @Email(message = "invalid email format")
     private String email;
+
     @NotEmpty(message = "role cannot be empty")
-    @Pattern(regexp = "^(Admin|Customer)$",
-            message = "role must be either Admin or Customer")
+    @Pattern(
+            regexp = "^(Admin|Customer)$",
+            message = "role must be either Admin or Customer"
+    )
     private String role;
+
     @NotNull(message = "balance cannot be null")
     @Positive(message = "balance must be positive")
-    private String balance;
+    private Double balance;
 
 }
